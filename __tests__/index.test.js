70e302aa06b03964451e0334f3fef6de13d65bfe
Rafael Caldeira <meet.rafaelsilva@gmail.com>
2023-09-20 22:58:03 -0300

    Adds PR workflow with tests and lint

---
+++ __tests__/index.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom'
+
+describe('Pipeline test', () => {
+  it('Runs tests', () => {
+    expect(true).toBeTruthy();
+  })
+
+  it('Runs tests #2', () => {
+    expect(false).toBeFalsy();
+  })
+
+  it('Runs tests #3', () => {
+    expect(1).toBe(1);
+  })
+})
