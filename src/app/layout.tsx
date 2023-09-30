8c7d7f6ddf90a24d40a6b9e185d378995d035efa
Rafael Caldeira <meet.rafaelsilva@gmail.com>
2023-09-29 22:29:20 -0300

    Fonts optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,18 @@ import GoogleAnalytics from "@/components/GoogleAnalytics";
 import Footer from "@/components/layout/footer/Footer";
 import Header from "@/components/layout/header/Header";
 import Content from "@/components/layout/Content";
+import { Inter } from 'next/font/google';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export default function RootLayout(
+  { children }: { children: React.ReactNode }
+) {
   return (
-    <html lang="pt" className="h-full font-inter">
+    <html lang="pt" className={`h-full font-inter ${inter.className}`}>
       <GoogleAnalytics gaMeasurementId={process.env.GA_MEASUREMENT_ID ?? ''}/>
 
       <body className="flex flex-col h-full">
